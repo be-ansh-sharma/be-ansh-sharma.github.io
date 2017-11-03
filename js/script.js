a28f77ab1f09a0d995921ac70d189f694e52e021
crazymaxstl <anshsharma60@gmail.com>
2017-11-03 20:49:04 +0530

    links updated

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 $(document).ready(function () {
 
-    
+    $('.sidenav a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, "fast", 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
     
     $('#opennav').on('click', function(event) {
         event.stopPropagation();        
@@ -28,16 +39,28 @@ $(document).ready(function () {
      });
 
      var divHeight = $('#jumbotron-container').outerHeight() + $('#skill-container').outerHeight();
+     var $form = $('#contact-container');
+     var formTop = $form.offset().top;
+     var formHeight = $form.height();
+     var $window = $(window);
+     var windowHeight = $window.height();
+
 
     $(window).on('scroll',function(){
-        var scrollTop = $(window).scrollTop();
-        //console.log("div height is " + divHeight + " and scroll is " + scrollTop);
-        //console.log( $('#jumbotron-container').outerHeight() );
+        var scrollTop = $window.scrollTop();
+        var windowBottom = scrollTop + windowHeight;
         if(scrollTop > divHeight) {
             $('#opennav').css('color' , 'black');
         }
         if(scrollTop < divHeight) {
             $('#opennav').css('color' , 'white');
+        }
+        if(windowBottom > formTop + (formHeight/2) ) {
+            $('#contact-container .row').addClass("animated").addClass("pop-in");
+        }
+
+        if(windowBottom < formTop) {
+            $('#contact-container .row').removeClass("animated").removeClass("pop-in");
         }
     });
    
